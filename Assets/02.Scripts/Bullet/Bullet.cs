4c719dcad9aa80770460358ad453682a27d44d57
mongilteacher <158262352+mongilteacher@users.noreply.github.com>
2025-11-04 11:19:05 +0900

    총알 속도

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour
     [Header("이동")] 
     public float StartSpeed   = 1f;
     public float EndSpeed     = 7f;
-    public float Acceleration = 1.2f;
+    public float Duration     = 1.2f;
     private float _speed;
 
     private void Start()
@@ -18,7 +18,12 @@ public class Bullet : MonoBehaviour
 
     private void Update()
     {
-        _speed += Time.deltaTime * Acceleration; // 초당 +1 * 1.2f;
+        // 목표: Duration 안에 EndSpeed까지 달성하고 싶다.
+        
+        // 논리적인 실수(1), 코딩 컨벤션(1)
+        float acceleration = (EndSpeed - StartSpeed) / Duration;
+        //                     6      / 1.2   = 5
+        _speed += Time.deltaTime * acceleration;   // 초당 + 1 * 가속도
         _speed  = Mathf.Min(_speed, EndSpeed);
         //         ㄴ 어떤 속성과 어떤 메서드를 가지고 있는지 톺아볼 필요가 있다.
         
