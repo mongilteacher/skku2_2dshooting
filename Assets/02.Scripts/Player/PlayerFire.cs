4c85f747bdebbb1d6848a6f71ea033d6f952f4ae
mongilteacher <158262352+mongilteacher@users.noreply.github.com>
2025-11-04 15:41:21 +0900

    2. 총알을 양 옆으로 2개 발사, 3. 총알 발사에 쿨타임 적용

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -10,22 +10,38 @@ public class PlayerFire : MonoBehaviour
 
     [Header("총구")]
     public Transform FirePosition;
+    public float FireOffset = 0.3f;
 
+    [Header("쿨타임")] 
+    public const float CoolTime = 0.6f;
+    private float _coolTimer;
+
+    
     private void Update()
     {
+        _coolTimer -= Time.deltaTime;
+        if (_coolTimer > 0) return;     // 조기 리턴
+        
         // 1. 발사 버튼을 누르고 있으면..
         if (Input.GetKey(KeyCode.Space))
         {
+            // 발사하고 나면 쿨타이머를 초기화
+            _coolTimer = CoolTime;
+                
             // 유니티에서 게임 오브젝트를 생성할때는 new가 instaintate 라는 메서드를 이용한다.
             // 클래스 -> 객체(속성+기능) -> 메모리에 로드된 객체를 인스턴스
             //                        ㄴ 인스턴스화
             
             // 2. 프리팹으로부터 총알(게임 오브젝트)을 생성한다.
-            GameObject bullet = Instantiate(BulletPrefab);
-            
+            GameObject bullet1 = Instantiate(BulletPrefab);
+            GameObject bullet2 = Instantiate(BulletPrefab);
+
             // 3. 총알의 위치를 총구 위치로 바꾸기 
-            bullet.transform.position = FirePosition.position;
+            bullet1.transform.position = FirePosition.position + new Vector3(-FireOffset, 0, 0);
+            bullet2.transform.position = FirePosition.position + new Vector3( FireOffset, 0, 0);
         }
+        
+       
     }
     
     
