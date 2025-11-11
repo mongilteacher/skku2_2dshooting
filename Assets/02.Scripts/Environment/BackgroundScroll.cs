1fe0bb8c3720ef0d65ba1df084329577d9a37232
mongilteacher <158262352+mongilteacher@users.noreply.github.com>
2025-11-11 13:41:46 +0900

    Feat: 배경오브젝트 시차 스크롤링 적용

--- Assets/02.Scripts/Environment/BackgroundScroll.cs
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -5,12 +5,14 @@ public class BackgroundScroll : MonoBehaviour
     // 목표: 배경 스크롤이 되도록 하고 싶다.
     
     // 필요 속성
-    // - 머터리얼
-    // - 스크롤 속도
+    public Material BackgroundMaterial;
+    public float ScrollSpeed = 0.1f;
 
     private void Update()
     {
         // 방향을 구한다.
+        Vector2 direction = Vector2.up; // (0, 1)
         // 움직인다.(스크롤한다.)
+        BackgroundMaterial.mainTextureOffset += direction * ScrollSpeed * Time.deltaTime;
     }
 }
