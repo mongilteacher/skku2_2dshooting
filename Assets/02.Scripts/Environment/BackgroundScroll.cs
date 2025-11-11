99cfa30ca52fe86746093008b2207c93d44597b5
mongilteacher <158262352+mongilteacher@users.noreply.github.com>
2025-11-11 13:15:10 +0900

    Feat: 배경 머터리얼 설정

---
+++ Assets/02.Scripts/Environment/BackgroundScroll.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    // 목표: 배경 스크롤이 되도록 하고 싶다.
+    
+    // 필요 속성
+    // - 머터리얼
+    // - 스크롤 속도
+
+    private void Update()
+    {
+        // 방향을 구한다.
+        // 움직인다.(스크롤한다.)
+    }
+}
