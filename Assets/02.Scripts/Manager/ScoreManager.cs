36301ca375f1d146610ee701913256bdf87450f5
mongilteacher <mongilteacher@gmail.com>
2025-11-12 10:51:28 +0900

    Feat. 에너미 죽일때마다 점수 Add

--- Assets/02.Scripts/Manager/ScoreManager.cs
+++ Assets/02.Scripts/Manager/ScoreManager.cs
@@ -3,18 +3,31 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // 목표: 적을 죽일 때마다 점수를 올리고, 현재 점수를 UI에 표시하고 싶다.
+    // 응집도를 높혀라
+    // 응집도 : '데이터'와 '데이터를 조작하는 로직'이 얼마나 잘 모였있냐
+    // 응집도를 높이고, 필요한 것만 외부에 공개하는 것을 '캡슐화'
     
-    // 필요 속성
-    // - 현재 점수 UI(Text 컴포넌트) (규칙: UI 요소는 항상 변수명 뒤에 UI 붙인다.)
-    public Text CurrentScoreTextUI;
-    // - 현재 점수를 기억할 변수
+    [SerializeField]
+    private Text _currentScoreTextUI;
     private int _currentScore = 0;
-
-
+    
     private void Start()
     {
-        CurrentScoreTextUI.text = $"현재 점수: {_currentScore}";
+        Refresh();
+    }
+
+    public void AddScore(int score)
+    {
+        if (score <= 0) return;
+        
+        _currentScore += score;
+        
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        _currentScoreTextUI.text = $"현재 점수: {_currentScore}";
     }
 
 }
